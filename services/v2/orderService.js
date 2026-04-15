68c786fd1ca8f022fe4b49028539dc104173a90a
Phuc-1234 <157017261+Phuc-1234@users.noreply.github.com>
2026-04-15 07:12:35 +0700

    fix null prop json bug

--- services/v2/orderService.js
+++ services/v2/orderService.js
@@ -15,7 +15,7 @@ const fetchUserCart = async (userId) => {
             items: [],
             payment: { method: "pending", info: {} },
             shipping: { method: "pending" },
-            recipientInfo: { fullName: user.fullName, address: user.address },
+            recipientInfo: { fullName: user.fullName || "Pending", address: user.address || "Pending" },
         });
         await cart.save();
     }
